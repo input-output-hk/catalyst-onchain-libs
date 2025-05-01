016b61209a9192f5278bd8276281195f2d611bc6
colll78 <philipdisarro@gmail.com>
2025-05-01 16:14:39 -0700

    fix dependencies

--- nix/project.nix
+++ nix/project.nix
@@ -8,7 +8,6 @@ let
     src = ../.;
     name = "plutarch-onchain-lib";
     compiler-nix-name = "ghc966";
-    # index-state = "2024-10-16T00:00:00Z";
     inputMap = {
       "https://chap.intersectmbo.org/" = inputs.CHaP;
     };
